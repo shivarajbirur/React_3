e39391539a9e73db15779068de8fa898907c1973
SHIVARAJ B S <128891870+shivarajbirur@users.noreply.github.com>
2023-07-13 15:37:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// ReactDOM.render(
+//   <div>
+//     <h1>Heading</h1>
+//     <p>Paragraph</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// var myElement = ( <div>
+//   <h1>Heading</h1>
+//   <p>Paragraph</p>
+// </div>);
+
+// var myElement = ( <div>
+//   <h1>Heading</h1>
+//   <p>5 + 2 = {5+2}</p>
+// </div>);
+
+const myElement = {
+ <ul>
+ <li>Apple</li>
+ </ul>
+};
+
+const myRoot = ReactDOM.createRoot(document.getElementById("root"));
+myRoot.render(myElement);
+
